ff6e6e3abd4f3f5481b63f299a637d3fd437179d
Adrian de Sanjosé Lopez <Adrian@alpha1.be>
2022-08-22 11:55:40 +0200

    y

---
+++ gui.py
@@ -0,0 +1,18 @@
+import sys
+import pygame as pg
+
+from grid import Grid
+
+class GUI:
+    def __init__(self):
+        self.grid = Grid()
+        pg.init()
+        self.screenWidth, self.screenHeight = 750, 750
+        self.screen = pg.set_mode(self.screenSize)
+    
+    @property
+    def screenSize(self):
+        return (self.screenWidth, self.screenHeight)
+    
+    def runGame(self):
+        pass
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from grid import Grid
 from solver import Solver
+from gui import GUI
 
 if __name__ == "__main__":
-    grid = Grid()
-    grid.initRandomGrid(0.4)
-    print(grid)
-    Solver(grid).bruteForce()
-    print(grid)
+    # grid = Grid()
+    # grid.initRandomGrid(0.4)
+    # print(grid)
+    # Solver(grid).bruteForce()
+    # print(grid)
+    gui = GUI()
